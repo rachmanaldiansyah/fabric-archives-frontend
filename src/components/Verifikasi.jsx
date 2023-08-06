2a0e3e3394e08403e05a7f51b6fcf9dd787a8825
RACHMAN21 <rachmanaldiansyah03@gmail.com>
2023-08-06 17:01:21 +0700

    Menambahkan fungsi untuk verifikasi arsip ijazah & sertifikat

--- src/components/Verifikasi.jsx
+++ src/components/Verifikasi.jsx
@@ -1,47 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { IoRibbonOutline, IoSchoolOutline } from "react-icons/io5";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const Verifikasi = () => {
+  const [hashValue, setHashValue] = useState("");
+  const [verificationResult, setVerificationResult] = useState(null);
+  const [verificationError, setVerificationError] = useState(null);
+
+  const verifyArsipIjazah = async () => {
+    try {
+      const verifyAsset = { method: "VerifyArsipIjazah", args: [hashValue] };
+      const verifyAssetResponse = await axios.post(
+        "http://localhost:5001/invoke/ijazah/chaincode-ijazah",
+        verifyAsset,
+        { withCredentials: true }
+      );
+      setVerificationResult(verifyAssetResponse.data);
+      setVerificationError(null); // Reset error message
+    } catch (error) {
+      console.error("Failed to verify asset:", error);
+      setVerificationResult(null);
+      setVerificationError("Data arsip ijazah siswa tidak ditemukan, nomor ijazah yang dimasukan tidak valid."); // Set error message
+    }
+  };
+
+  const verifyArsipSertifikat = async () => {
+    try {
+      const verifyAsset = {
+        method: "VerifyArsipSertifikat",
+        args: [hashValue],
+      };
+      const verifyAssetResponse = await axios.post(
+        "http://localhost:5002/invoke/sertifikat/chaincode-sertifikat",
+        verifyAsset,
+        { withCredentials: true }
+      );
+      setVerificationResult(verifyAssetResponse.data);
+      setVerificationError(null);
+    } catch (error) {
+      console.error("Failed to verify asset:", error);
+      setVerificationResult(null);
+      setVerificationError("Data arsip sertifikat siswa tidak ditemukan, nomor sertifikat yang dimasukan tidak valid.");
+    }
+  };
+
   return (
-    <section className="hero has-background-grey-lighter is-fullheight is-fullwidth">
+    <section className="hero has-background-grey-lighter is-fullheight">
       <div className="hero-body">
         <div className="container has-text-centered">
-          <div className="column is-6 is-offset-3">
+          <div className="column is-10 is-offset-1">
             <h1 className="title">Verifikasi Dokumen</h1>
             <h2 className="subtitle">
-              Silahkan melakukan verifikasi dengan memasukan hash dari dokumen
+              Silahkan melakukan verifikasi dengan memasukkan hash dari dokumen
               ijazah atau sertifikat siswa.
             </h2>
-            <div className="container box">
-              <div className="field is-grouped">
-                <p className="control is-expanded">
+            <div className="box">
+              <div className="field has-addons">
+                <div className="control is-expanded">
                   <input
                     className="input"
                     type="text"
-                    placeholder="Masukkan Hash dari dokumen ijazah/sertifikat siswa"
+                    placeholder="Masukkan nomor ijazah atau sertifikat siswa"
+                    value={hashValue}
+                    onChange={(e) => setHashValue(e.target.value)}
                   />
-                </p>
-                <p className="control">
-                  <button className="button is-info">Verifikasi</button>
-                </p>
+                </div>
+                <div className="control">
+                  <button
+                    className="button is-info"
+                    onClick={verifyArsipIjazah}
+                  >
+                    <IoSchoolOutline />
+                  </button>
+                </div>
+                <div className="control">
+                  <button
+                    className="button is-info"
+                    onClick={verifyArsipSertifikat}
+                  >
+                    <IoRibbonOutline />
+                  </button>
+                </div>
               </div>
             </div>
 
-            <div className="table-container box">
-              <table className="table is-striped is-hoverable is-fullwidth">
-                <thead>
-                  <tr>
-                    <th>No Ijazah</th>
-                    <th>NISN</th>
-                    <th>Nama Siswa</th>
-                    <th>Jenis Kelamin</th>
-                    <th>Program Studi</th>
-                    <th>Arsip Ijazah</th>
-                    <th>Status</th>
-                  </tr>
-                </thead>
-                <tbody></tbody>
-              </table>
-            </div>
+            {verificationError && (
+              <div className="notification is-danger">{verificationError}</div>
+            )}
+
+            {verificationResult && (
+              <div className="box">
+                <h3 className="subtitle">Hasil Verifikasi:</h3>
+                <SyntaxHighlighter language="json" style={atomDark}>
+                  {JSON.stringify(verificationResult, null, 2)}
+                </SyntaxHighlighter>
+              </div>
+            )}
           </div>
         </div>
       </div>
